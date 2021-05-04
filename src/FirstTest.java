ba897650fcc0fb1e7c9795e6bf3a20fe7d16d7ff
Artem <artemii-gold@yandex.ru>
2021-05-04 17:07:42 +0300

    Update on 4 may 2021 (2)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,6 +1,7 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import org.junit.After;
 import org.junit.Assert;
@@ -14,6 +15,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
 
 public class FirstTest {
     private AppiumDriver driver;
@@ -176,8 +178,13 @@ public void firstTest(){
     }
     @Test
     public void testSwipeUp(){
+
+        swipeScreen(Direction.LEFT);
+        swipeScreen(Direction.LEFT);
+        swipeScreen(Direction.LEFT);
+
         waitForElementByAndClick(
-                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                By.id("org.wikipedia:id/fragment_onboarding_done_button"),
                 "error: element skip_button - not found",
                 5
         );
@@ -200,22 +207,87 @@ public void firstTest(){
                 "error: JavaServer Pages - not found",
                 15
         );
-        swipeUp(2000);
-        swipeUp(2000);
-        swipeUp(2000);
-        swipeUp(2000);
+        swipeScreen(Direction.UP);
+        swipeScreen(Direction.UP);
     }
 
 //============================Methods==============================================
+public void swipeScreen(Direction dir) {
+    System.out.println("swipeScreen(): dir: '" + dir + "'"); // always log your actions
 
-    protected void swipeUp(int timeOfSwipe){
+    // Animation default time:
+    //  - Android: 300 ms
+    //  - iOS: 200 ms
+    // final value depends on your app and could be greater
+    final int ANIMATION_TIME = 2000; // ms
+
+    final int PRESS_TIME = 2000; // ms
+
+    int edgeBorder = 10; // better avoid edges
+    PointOption pointOptionStart, pointOptionEnd;
+
+    // init screen variables
+    Dimension dims = driver.manage().window().getSize();
+
+    // init start point = center of screen
+    pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
+
+    switch (dir) {
+        case DOWN: // center of footer
+            pointOptionEnd = PointOption.point(dims.width / 2, dims.height - edgeBorder);
+            break;
+        case UP: // center of header
+            pointOptionEnd = PointOption.point(dims.width / 2, edgeBorder);
+            break;
+        case LEFT: // center of left side
+            pointOptionEnd = PointOption.point(edgeBorder, dims.height / 2);
+            break;
+        case RIGHT: // center of right side
+            pointOptionEnd = PointOption.point(dims.width - edgeBorder, dims.height / 2);
+            break;
+        default:
+            throw new IllegalArgumentException("swipeScreen(): dir: '" + dir + "' NOT supported");
+    }
+
+    // execute swipe using TouchAction
+    try {
+        new TouchAction(driver)
+                .press(pointOptionStart)
+                // a bit more reliable when we add small wait
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(PRESS_TIME)))
+                .moveTo(pointOptionEnd)
+                .release().perform();
+    } catch (Exception e) {
+        System.err.println("swipeScreen(): TouchAction FAILED\n" + e.getMessage());
+        return;
+    }
+
+    // always allow swipe action to complete
+    try {
+        Thread.sleep(ANIMATION_TIME);
+    } catch (InterruptedException e) {
+        // ignore
+    }
+}
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT;
+    }
+
+    //==========================================================================
+
+    protected void swipeUp(int timeOfSwipe){            //ДОРЕШАТЬ НАДО
         TouchAction action = new TouchAction(driver);
         Dimension size = driver.manage().window().getSize();
         int x = size.height / 2;
         int start_y = (int) (size.height * 0.8);
         int end_y = (int) (size.width * 0.2);
 
-        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+        //action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+        action.tap(PointOption.point(x,start_y)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(timeOfSwipe))).moveTo(PointOption.point(x,end_y)).release().perform();
 
     }
 
